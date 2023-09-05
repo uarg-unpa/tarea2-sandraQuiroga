be1cc6d087f874cc723dba1b1d2bc0b7040b441d
sandraQuiroga <31734763+sandraQuiroga@users.noreply.github.com>
2023-09-05 12:32:25 +0000

    actualizacion

---
+++ entrega/semana _I/Ejercicio_3.py
@@ -0,0 +1,20 @@
+#Escribir un programa que cree dos variables, le cargue números arbitrarios (cualquiera) y muestre por pantalla: 
+# la suma.
+# la resta.
+# la multiplicación.
+# la división entera.
+# el resto de la división entera.
+# cada operación por línea.
+
+# importing the random module
+import random
+
+num_1=randint(0,9)
+num_2=random.random()
+print(f"num_1 {num_1}")
+suma=num_1 + num_2
+resta=num_1 - num_2
+multiplicacion=num_1 * num_2
+division=num_1/num_2
+division=num_1%num_2
+print("No te olvides de hacer los commits")
